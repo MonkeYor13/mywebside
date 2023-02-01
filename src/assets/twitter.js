af57101591b55311cd1e4ea7d6f09b29c526c61b
MonkeYor13 <monkeyor13@gmail.com>
2023-02-01 18:27:24 -0300

    img

---
+++ src/assets/twitter.js
@@ -0,0 +1,12 @@
+// importar imagenes
+import twitter from "../assets/twitter.svg";
+// import instagram from "../assets/instagram.svg";
+// import linkedin from "../assets/linkedin.svg";
+
+export default {
+  "img1": twitter
+}
+
+
+
+
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import "../css/SocialMedia.css";
 
 // importar imagenes
-import twitter from "../assets/twitter.svg";
+// import twitter from "../assets/twitter.svg";
 // import instagram from "../assets/instagram.svg";
 // import linkedin from "../assets/linkedin.svg";
-
+// import TwitterIcon from "../assets/twitter";
+import twitter from '../assets/twitter';
 
 class SocialMedia extends Component {
   render() {
@@ -13,7 +14,10 @@ class SocialMedia extends Component {
       <div className="social-media">
         <ul>
           <li>
-            <img className="imgSvg" src={twitter} alt="logo" />
+          {/* <TwitterIcon /> */}
+
+            {/* <img className="imgSvg" src={twitter} alt="logo" /> */}
+            <img className="imgSvg" src={twitter.img1} alt="logo" />
           </li>
           {/* <li>
             <img className="logo" src={instagram} alt="logo" />
